e5d0e1e2af24015b8a25413722b3994e23858efc
mogog97 <mario.mog.111@gmail.com>
2022-02-11 13:07:58 +0100

    Secod commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'proyecto1';
+  title:string = "Contador";
+  num:number = 0;
 }
