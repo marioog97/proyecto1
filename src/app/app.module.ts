da086261f285ae1438034e6d3bbed873d01b5e23
mogog97 <mario.mog.111@gmail.com>
2022-02-14 12:28:50 +0100

    c14

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; 
 
 import { AppComponent } from './app.component';
 import { PrincipalComponent } from './principal/principal.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HijoPrComponent } from './hijo-pr/hijo-pr.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrincipalComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HijoPrComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [PrincipalComponent]
--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrincipalComponent implements OnInit {
 
+
+  personaData:any = {nombre:"Maria", apellidos: "De la O", edad: 25};
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  eventoBotonAngular():void{
+
+    console.log("Evento: " + this.personaData.nombre+ " " + this.personaData.apellidos + " y tiene " + this.personaData.edad);
   }
 
 }
