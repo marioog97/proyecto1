895aa1d51eb484ed926f83383a5966a95b373982
mogog97 <mario.mog.111@gmail.com>
2022-02-15 10:32:48 +0100

    Fin tema 9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; 
 
+
 import { AppComponent } from './app.component';
 import { PrincipalComponent } from './principal/principal.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HijoPrComponent } from './hijo-pr/hijo-pr.component';
+import { CSeccion2Component } from './c-seccion2/c-seccion2.component';
+import { CSeccion3Component } from './c-seccion3/c-seccion3.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes=[
+  {path:'', component: CSeccion2Component},
+  {path:'seccion2', component: CSeccion2Component}, 
+  {path:'seccion3', component: CSeccion3Component},
+  {path:'seccion/:nombre', component: CSeccion2Component},
+  {path:'seccion3/:ciudad/:poblacion', component: CSeccion3Component}
+];
 
 @NgModule({
   declarations: [
@@ -14,11 +26,15 @@ import { HijoPrComponent } from './hijo-pr/hijo-pr.component';
     PrincipalComponent,
     HeaderComponent,
     FooterComponent,
-    HijoPrComponent
+    HijoPrComponent,
+    CSeccion2Component,
+    CSeccion3Component
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
+    
   ],
   providers: [],
   bootstrap: [PrincipalComponent]
